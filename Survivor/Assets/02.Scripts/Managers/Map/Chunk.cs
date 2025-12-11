e24237ee95691c1a9722cff11683ec9d4ad7824e
naneo10 <144968440+naneo10@users.noreply.github.com>
2025-12-11 16:58:41 +0900

    first update
    create background per seconds
    chunkManager.cs
    mapMove.cs
    mapFollowCam.cs

--- Survivor/Assets/02.Scripts/Managers/Map/Chunk.cs
+++ Survivor/Assets/02.Scripts/Managers/Map/Chunk.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChunkManager : MonoBehaviour
+public class Chunk : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
