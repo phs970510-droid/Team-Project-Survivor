0a3ac8bacba306314ac936a7c875b4ab6c727a9c
태완 <ckxodhks1234@gmail.com>
2025-12-12 12:14:14 +0900

    1212_Commit

--- Survivor/Assets/02.Scripts/DataSO/WeaponData.cs
+++ Survivor/Assets/02.Scripts/DataSO/WeaponData.cs
@@ -5,9 +5,12 @@ public class WeaponData : ScriptableObject
 {
     [Header("무기 설정")]
     public GameObject weaponPrefab;
+    public string weaponName;
+    public bool isLocked;
     public float damage = 10f;
     public float speed = 10f;
     public float fireCoolTime = 1.0f;
     public int bulletCount = 1;
     public float attackRange = 10f;
+    public int price;
 }
