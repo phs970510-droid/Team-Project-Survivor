c899b00c03bde6cf0c10c7802bb19fb1ef6de40b
mobaaamo <kwy5190@gmail.com>
2025-12-20 05:23:59 +0900

    feat : Move to next stage

--- Survivor/Assets/02.Scripts/BaseScene/StageSelect/StageMovement.cs
+++ Survivor/Assets/02.Scripts/BaseScene/StageSelect/StageMovement.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class StageMovement : MonoBehaviour
 {
     public StageSceneLode stageSceneLode;
+    public ChunkManager chunkManager;
+
+    private void Awake()
+    {
+        chunkManager = FindObjectOfType<ChunkManager>();
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Main"))
         {
             stageSceneLode.MainSceneLoder();
         }
-        if (other.CompareTag("Tutorial"))
+        else if (other.CompareTag("Tutorial"))
         {
             stageSceneLode.TutorialSceneLoader();
+        }
+        else if (other.CompareTag("Stage1"))
+        {
+            chunkManager.SelectMap(2, 1);
+            stageSceneLode.Stage1SceneLodar();
+        }
+        else if (other.CompareTag("Stage2"))
+        {
+            chunkManager.SelectMap(2, 2);
+            stageSceneLode.Stage2SceneLodar();
+        }
+        else if (other.CompareTag("Stage3"))
+        {
+            chunkManager.SelectMap(2, 3);
+            stageSceneLode.Stage3SceneLodar();
         }
 
 
